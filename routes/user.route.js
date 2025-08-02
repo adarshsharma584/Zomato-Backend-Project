c55106f657baef311f7ba667a93e1712af80725f
adarshsharma584 <adarshsharma0862@gmail.com>
2025-08-02 23:23:09 +0530

    add user routes

---
+++ routes/user.route.js
@@ -0,0 +1,9 @@
+import {Router} from "express";
+import {registerUser,login,logout} from "../controllers/user.controller.js"
+const router = Router();
+
+router.post("/register",registerUser);
+router.post("/login",login);
+router.post("/logout",logout);
+
+export default  router;
